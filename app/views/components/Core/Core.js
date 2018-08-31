2bfacc9394ea660834c2f809e0749a346dd1aea7
Kapil <kapilbindal1@gmail.com>
2018-08-31 15:36:06 +0530

    minor changes

--- app/views/components/Core/Core.js
+++ app/views/components/Core/Core.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { callbackify } from 'util';
 export const Progress = (props) => {
     let barWidth = 0;
-    if(props.type === 'theme-blue'){ barWidth=props.value + '%'; }
-    if(props.type === 'theme-red-Yellow-green')
-    {
-        //  barWidth = `calc(100% - ${(props.value + '%')} )` 
-        barWidth=100 - props.value;
-        barWidth=barWidth + '%';
+    if (props.type === 'theme-blue') { barWidth = props.value + '%'; }
+    if (props.type === 'theme-red-Yellow-green') {
+        barWidth = 100 - props.value;
+        barWidth = barWidth + '%';
     }
-    return(
+    return (
         <div className={`cs-progress ${props.className} ${props.type}`}>
-        {/* {props.type === 'theme-blue' && <div className="bar"></div>}
-        {props.type === 'theme-red-Yellow-green' && <div className="bar"></div>} */}
-            <div className="bar"
-            
-          style={{
-                width: barWidth,
-            }}
-
-            ></div>
-        
+            <div className="bar" style={{ width: barWidth, }} ></div>
         </div>
     );
 }
