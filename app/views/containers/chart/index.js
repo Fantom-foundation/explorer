650c6c4d0e6a01e75ac544c446ee0fe070b8d931
Komal <kgoyal@innow8apps.com>
2018-09-13 12:56:44 +0530

    tooltip

--- app/views/containers/chart/index.js
+++ app/views/containers/chart/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 const data = {
   labels: ['10.00AM', '12.00PM', '2:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM'],
@@ -15,6 +15,7 @@ const data = {
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
       pointBorderColor: 'rgba(189, 16, 224,1)',
+      name: 'Market Cap',
       // pointBackgroundColor: '#bd10e0',
       // pointBorderWidth: 1,
       // pointHoverRadius: 7,
@@ -22,19 +23,20 @@ const data = {
      // pointHoverBorderColor: 'rgba(189, 16, 224,1)',
      // pointHoverBorderWidth: 2,
      // pointRadius: 1,
-      //pointHitRadius: 1,
+      // pointHitRadius: 1,
       data: [65, 59, 80, 81, 56, 55, 40, 70, 20, 25, 31],
     },
     {
       fill: false,
       lineTension: 0.1,
-      //backgroundColor: 'rgba(0,0,255)',
+      // backgroundColor: 'rgba(0,0,255)',
       borderColor: 'rgba(3, 155, 229,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
       pointBorderColor: 'rgba(0,0,255,1)',
+      name: 'Price(USD)',
       // pointBackgroundColor: '#fff',
       // pointBorderWidth: 1,
       // pointHoverRadius: 5,
@@ -69,12 +71,12 @@ const options = {
       ticks: {
         fontSize: 18,
         fontFamily: 'Gotham-Medium',
-      //fontColor: 'rgb()',
+      // fontColor: 'rgb()',
         maxRotation: 0.00001,
         padding: 10,
-        //labelOffset: 20,
+        // labelOffset: 20,
         callback(value, index) {
-          console.log(value,index, "value");
+          console.log(value, index, 'value');
           if (value === 0) { return '0'; }
           if (value === 20) {
             return '25';
@@ -96,7 +98,7 @@ const options = {
         color: 'grey',
       },
     }],
- 
+
     xAxes: [{
       crosshair: {
         width: 0.5,
@@ -105,8 +107,8 @@ const options = {
       },
       ticks: {
        // fontSize: 18,
-        //fontFamily: 'Gotham-Medium',
-        //fontColor: colorMap['primaryBlue'],
+        // fontFamily: 'Gotham-Medium',
+        // fontColor: colorMap['primaryBlue'],
         maxRotation: 0.00001,
         padding: 10,
         labelOffset: 60,
@@ -127,8 +129,8 @@ const options = {
       angleLines: {
         color: 'transparent',
       },
- 
-    }]
+
+    }],
   },
   tooltips: {
     mode: 'index',
@@ -138,28 +140,28 @@ const options = {
     cornerRadius: 0,
     caretSize: 0,
     xPadding: 70,
+    intersect: true,
+    axis: 'y',
     yPadding: 25,
     titleFontColor: 'rgba(0, 0, 0, 0.87)',
     titleFontSize: 10,
     titleFontFamily: 'Roboto',
     bodyFontFamily: 'Roboto',
     enabled: true,
+    crosshair: {
+      enabled: true,
+      width: 0.5,
+        zIndex: 50,
+        color: 'red',
+    },
     callbacks: {
-                        label: function (tooltipItem, data) {
-                            var amount = tooltipItem.yLabel + ' ' + 'USD';
-                            return amount
-                        }
-},
+      label(tooltipItem, data) {
+        const name = data.datasets[tooltipItem.datasetIndex].name;
+        const marketCap = `${name}: ${tooltipItem.yLabel} ` + `${name === 'Market Cap' ? 'USD' : ''}`;
+        return marketCap;
+      },
+    },
   },
-//   tooltips: {
-//     enabled: true,
-//     mode: 'single',
-//     callbacks: {
-//         label: function(tooltipItems, data) { 
-//             return tooltipItems.yLabel + ' €';
-//         }
-//   },
-// },
 };
 
 export default class Chart extends React.Component {
@@ -167,9 +169,11 @@ export default class Chart extends React.Component {
   render() {
     return (
       <div>
-        <Line options={options}
-        data={data}
-        height={100}/>
+        <Line
+          options={options}
+          data={data}
+          height={100}
+        />
       </div>
     );
   }
