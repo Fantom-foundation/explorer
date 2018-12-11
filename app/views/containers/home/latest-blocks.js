febebd4ddf4b8c9bc04a76621febeff8f09af317
Ankit Pandey <apandey@innow8apps.com>
2018-12-11 18:39:00 +0530

    -  latest transaction & block

--- app/views/containers/home/latest-blocks.js
+++ app/views/containers/home/latest-blocks.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {
-    Row,
-    Col,
-} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import _ from 'lodash';
 import moment from 'moment';
 import blockIcon from 'images/icons/block-icon.svg';
 import { Title } from 'views/components/coreComponent/index';
-
+import HttpDataProvider from '../../../../app/utils/httpProvider';
 
 export default class LatestBlocks extends React.Component {
   constructor(props) {
@@ -20,57 +17,125 @@ export default class LatestBlocks extends React.Component {
    * @api_key: send private key for security purpose
    * here call a api get-blocks and get data from blocks table.
    */
-  componentWillMount() {
-    fetch(
-      'http://localhost:3000/api/get-blocks',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          api_key: 'qscvfgrtmncefiur2345',
+  // componentWillMount() {
+  //   fetch(
+  //     'http://localhost:3000/api/get-blocks',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         api_key: 'qscvfgrtmncefiur2345',
+  //       },
+  //     },
+  //   )
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     this.setState({ blockArray: res.result });
+  //   }).catch((error) => {
+  //     console.log('error is !!!', error);
+  //   });
+  // }
+
+  componentDidMount() {
+    HttpDataProvider.post('http://18.216.205.167:5000/graphql?', {
+      query: `
+          {
+            blocks(first:10) {
+              pageInfo {
+                hasNextPage
+              }
+              edges {
+                cursor
+                node {
+                  payload
+                }
+              }
+            }
+          }`,
+    })
+      .then(
+        (res) => {
+          if (res && res.data) {
+            // this.formatTransactionList(res.data);
+            const allBlockData = [];
+            const edges = res.data.data.blocks.edges;
+            let cursor;
+            edges.forEach((val) => {
+              const { hash, index, stateHash, transactions } = val.node.payload;
+              cursor = val.cursor;
+              allBlockData.push({
+                hash,
+                height: index,
+                parentHash: stateHash,
+                transactions: transactions.length,
+              });
+            });
+            this.setState({
+              blockArray: allBlockData,
+              cursor,
+            });
+            console.log('allBlockData', allBlockData);
+          }
+          return null;
         },
-      },
-    )
-    .then((res) => res.json())
-    .then((res) => {
-      this.setState({ blockArray: res.result });
-    }).catch((error) => {
-      console.log('error is !!!', error);
-    });
+        () => {
+          console.log('1');
+        }
+      )
+      .catch((err) => {
+        console.log(err, 'err in graphql');
+      });
   }
   render() {
     const blocks = this.state.blockArray;
     return (
       <Col xs={12} md={6} className="right">
         <Row className="header bg-white mb-3 pt-2 pb-3">
-          <Col><Title h2 className="text-uppercase mb-0">Latest Blocks</Title></Col>
-          <Col className="link-column"><a href="/blocks" className="link pt-2 text-black">View all</a></Col>
+          <Col>
+            <Title h2 className="text-uppercase mb-0">
+              Latest Blocks
+            </Title>
+          </Col>
+          <Col className="link-column">
+            <a href="/blocks" className="link pt-2 text-black">
+              View all
+            </a>
+          </Col>
         </Row>
         <Row className="blocks">
-          {blocks && blocks.length && blocks.length > 0 && blocks.map((data, index) => (
-            <Col key={index} xs={12} className="bg-white mb-3">
-              <Row>
-                <Col className="pr-0">
-                  <p className="text-black"><img src={blockIcon} className="block-icon" />{data.block_number}</p>
-                </Col>
-                <Col className="time-date-col pl-0">
+          {blocks &&
+            blocks.length &&
+            blocks.length > 0 &&
+            blocks.map((data, index) => (
+              <Col key={index} xs={12} className="bg-white mb-3">
+                <Row>
+                  <Col className="pr-0">
+                    <p className="text-black">
+                      <img src={blockIcon} className="block-icon" />
+                      {data.block_number}
+                    </p>
+                  </Col>
+                  {/* <Col className="time-date-col pl-0">
                   <p><span className="text-primary">{moment(parseInt(data.timestamp, 10)).fromNow()}</span></p>
-                </Col>
-              </Row>
-              <p className="hash-holder">
-                <span className="text-gray">Hash</span>&nbsp;
-                <span className="text-primary hash-value">{data.hash}</span>
-              </p>
-              <p>
-                <span className="text-gray">Mined by</span>&nbsp;
-                <span className="text-primary">John Doe</span>
-              </p>
-              <p className="mb-0">
-                <span className="text-gray">Txns</span>&nbsp;
-                <span className="text-primary">{data.size}</span>
-              </p>
-            </Col>
-         ))}
+                </Col> */}
+                </Row>
+                <p className="hash-holder">
+                  <span className="text-gray">Hash</span>
+                  &nbsp;
+                  <span className="text-primary hash-value">{data.hash}</span>
+                </p>
+                <p>
+                  <span className="text-gray">Mined by</span>
+                  &nbsp;
+                  <span className="text-primary">John Doe</span>
+                </p>
+                <p className="mb-0">
+                  <span className="text-gray">Txns</span>
+                  &nbsp;
+                  <span className="text-primary">{data.size}</span>
+                </p>
+              </Col>
+            ))}
         </Row>
       </Col>
     );
--- app/views/containers/home/latest-transactions.js
+++ app/views/containers/home/latest-transactions.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-    Row,
-    Col,
-} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import _ from 'lodash';
 import moment from 'moment';
 import { Title } from 'views/components/coreComponent/index';
 import { addLocaleData } from 'react-intl';
+import HttpDataProvider from '../../../../app/utils/httpProvider';
 
 export default class LatestTransactions extends React.Component {
   constructor(props) {
@@ -19,59 +17,160 @@ export default class LatestTransactions extends React.Component {
    * @api_key: send private key for security purpose
    * here call a api get-transactions and get data from transactions table.
    */
-  componentWillMount() {
-    fetch(
-      'http://localhost:3000/api/get-transactions',
+  // componentWillMount() {
+  //   fetch(
+  //     'http://localhost:3000/api/get-transactions',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         api_key: 'qscvfgrtmncefiur2345',
+  //         limit: 5,
+  //       },
+  //     },
+  //   )
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     this.setState({ transactionArray: res.result });
+  //   }).catch((error) => {
+  //     console.log('error is !!!', error);
+  //   });
+  // }
+
+  componentDidMount() {
+    HttpDataProvider.post('http://18.216.205.167:5000/graphql?', {
+      query: `
       {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          api_key: 'qscvfgrtmncefiur2345',
-          limit: 5,
+        transactions(first:10) {
+          pageInfo {
+            hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              hash
+              from
+              to
+              block
+              value
+              gas
+              cumulative
+              contract
+              root
+            }
+          }
+        }
+      }`,
+    })
+      .then(
+        (res) => {
+          if (res && res.data) {
+            // this.formatTransactionList(res.data);
+            const allTransactionData = [];
+            const edges = res.data.data.transactions.edges;
+            let cursor;
+            edges.forEach((val) => {
+              const {
+                block,
+                from,
+                hash,
+                to,
+                value,
+                gas,
+                cumulative,
+                contract,
+                root,
+              } = val.node;
+              cursor = val.cursor;
+
+              allTransactionData.push({
+                block_id: block,
+                address_from: from,
+                transaction_hash: hash,
+                address_to: to,
+                value,
+                gasUsed: gas,
+                cumulativeGasUsed: cumulative,
+                contractAddress: contract,
+                root,
+              });
+            });
+            this.setState({
+              transactionArray: allTransactionData,
+              cursor,
+            });
+            console.log('allTransactionData', allTransactionData);
+          }
+          return null;
         },
-      },
-    )
-    .then((res) => res.json())
-    .then((res) => {
-      this.setState({ transactionArray: res.result });
-    }).catch((error) => {
-      console.log('error is !!!', error);
-    });
+        () => {
+          console.log('1');
+        }
+      )
+      .catch((err) => {
+        console.log(err, 'err in graphql');
+      });
   }
   render() {
     const transactions = this.state.transactionArray;
     return (
-      <Col xs={12} md={6} className="left" >
+      <Col xs={12} md={6} className="left">
         <Row className="header bg-white mb-3 pt-2 pb-3">
-          <Col><Title h2 className="text-uppercase mb-0">Latest Transactions</Title></Col>
-          <Col className="link-column"><a href="/transactions" className="link pt-2 text-black">View all</a></Col>
+          <Col>
+            <Title h2 className="text-uppercase mb-0">
+              Latest Transactions
+            </Title>
+          </Col>
+          <Col className="link-column">
+            <a href="/transactions" className="link pt-2 text-black">
+              View all
+            </a>
+          </Col>
         </Row>
         <Row className="blocks">
-          {transactions && transactions.length && transactions.length > 0 && transactions.map((data, index) => (
-            <Col key={index} xs={12} className="bg-white  mb-3">
-              <Row>
-                <Col className="pr-0">
-                  <p className="tx-holder">
-                    <span className="text-black">TX#</span>&nbsp;
-                    <span className="text-primary tx-value">{data.transaction_hash}</span>
-                  </p>
-                </Col>
-                <Col className="time-date-col pl-0">
-                  <p><span className="text-primary">{moment(parseInt(data.createdAt, 10)).fromNow()}</span></p>
-                </Col>
-              </Row>
-              <p className="pb-2 mb-1 from-to-holder">
-                <span className="text-gray">From</span>&nbsp;
-                <span className="text-primary from-value">{data.address_from}</span>&nbsp;
-    
-                <span className="text-gray">to</span>&nbsp;
-                <span className="text-primary to-value">{data.address_to}</span>
-              </p>
-              <p className="mb-0">
-                <span className="text-gray">Amount 2.9999</span>&nbsp;
-                <span className="text-primary">Fantom</span>
-              </p>
-            </Col>))}
+          {transactions &&
+            transactions.length &&
+            transactions.length > 0 &&
+            transactions.map((data, index) => (
+              <Col key={index} xs={12} className="bg-white  mb-3">
+                <Row>
+                  <Col className="pr-0">
+                    <p className="tx-holder">
+                      <span className="text-black">TX#</span>
+                      &nbsp;
+                      <span className="text-primary tx-value">
+                        {data.transaction_hash}
+                      </span>
+                    </p>
+                  </Col>
+                  <Col className="time-date-col pl-0">
+                    <p>
+                      {/* <span className="text-primary">
+                        {moment(parseInt(data.createdAt, 10)).fromNow()}
+                      </span> */}
+                    </p>
+                  </Col>
+                </Row>
+                <p className="pb-2 mb-1 from-to-holder">
+                  <span className="text-gray">From</span>
+                  &nbsp;
+                  <span className="text-primary from-value">
+                    {data.address_from}
+                  </span>
+                  &nbsp;
+                  <span className="text-gray">to</span>
+                  &nbsp;
+                  <span className="text-primary to-value">
+                    {data.address_to}
+                  </span>
+                </p>
+                <p className="mb-0">
+                  <span className="text-gray">Amount 2.9999</span>
+                  &nbsp;
+                  <span className="text-primary">Fantom</span>
+                </p>
+              </Col>
+            ))}
         </Row>
       </Col>
     );
