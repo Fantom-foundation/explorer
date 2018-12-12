01c99ece01dafbbd21ecac6d94bac8efeb21aa9e
Razat Verma <rverma@innow8apps.com>
2018-12-12 19:22:10 +0530

    header desktop ui design

--- app/views/components/header/nav/navigation.js
+++ app/views/components/header/nav/navigation.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { isUserLoggedIn } from 'common/utility';
-import Logo from 'images/Logo/logo.png';
+import Logo from 'images/Logo/main-logo.svg';
 import Login from 'views/containers/login/index';
 import SettingIcon from 'images/icons/setting.svg';
 import { NavLink as RouterNavLink } from 'react-router-dom';
-
-
+import avatar from 'images/icons/avatar.svg';
 import {
   Collapse,
   Navbar,
@@ -15,6 +14,10 @@ import {
   NavItem,
   NavLink,
   Container,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 
 export default class Navigation extends React.Component {
@@ -50,14 +53,15 @@ export default class Navigation extends React.Component {
       return <Login toggleModal={this.toggleModal} modal={this.state.modal} />;
     }
     return (
-      <Navbar color="dark" dark expand="md">
-        <Container>
+      <Container>
+      <Navbar  dark expand="md">
+    
           <NavbarBrand tag={RouterNavLink} to="/"><img className="logo" src={Logo} /></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink tag={RouterNavLink} to="/">Home</NavLink>
+                <NavLink exact tag={RouterNavLink} to="/">Home</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink tag={RouterNavLink} to="/blocks">Blocks</NavLink>
@@ -68,13 +72,34 @@ export default class Navigation extends React.Component {
               <NavItem>
                 <NavLink tag={RouterNavLink} to="/address">Addresses</NavLink>
               </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  More
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <img src={avatar} />
+              </NavItem>
               {/* <NavItem>
               {isLoggedIn ? <NavLink onClick={(event) => this.logout(event)}>Logout</NavLink> : <NavLink onClick={(event) => this.toggleModal(event, this.state.modal)}>Login</NavLink> }
               </NavItem> */}
             </Nav>
+            
           </Collapse>
-        </Container>
       </Navbar>
+      </Container>
     );
   }
 }
