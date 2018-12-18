51a2e051d3d3c3926276e609aa43e4e66dea3151
Ankit Pandey <apandey@innow8apps.com>
2018-12-18 14:36:54 +0530

    footer html

--- app/views/components/footer/footer.js
+++ app/views/components/footer/footer.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import GithubIcon from '../../../images/social/github.svg';
+import LinkedInIcon from '../../../images/social/linked-in.svg';
+import SlackIcon from '../../../images/social/slack.svg';
 
 export default class Footer extends React.Component {
-  
   render() {
     return (
-<footer>
-<Container>
-  <hr />
-    <Row>
-        <Col>
-        <span className="text-white" style={{opacity:.7}}>Copyright © 2018 FANTOM. All Rights Reserved.</span>
-        </Col>
-        <Col className="text-right">
-        <span>Ca #</span>
-        </Col>
-        </Row>
+      <footer>
+        <Container>
+          <hr />
+          <Row className="menus">
+            <Col md={4}>
+              <div>
+                <h2 className="text-uppercase">Support</h2>
+                <a href="mailto:contact@fantom.foundation">
+                  contact@fantom.foundation
+                </a>
+              </div>
+            </Col>
+            <Col md={4}>
+              <div>
+                <h2 className="text-uppercase">Channel</h2>
+                <div className="social">
+                  <a href="#">
+                    <img src={GithubIcon} />
+                    Github
+                  </a>
+                  <a href="#">
+                    <img src={SlackIcon} />
+                    Slack
+                  </a>
+                  <a href="#">
+                    <img src={LinkedInIcon} />
+                    Linkedin
+                  </a>
+                </div>
+              </div>
+            </Col>
+            <Col md={4} />
+          </Row>
+          <Row>
+            <Col>
+              <span className="text-white">
+                Copyright © 2018 FANTOM. All Rights Reserved.
+              </span>
+            </Col>
+          </Row>
         </Container>
-  </footer>
-   
+      </footer>
     );
   }
 }
