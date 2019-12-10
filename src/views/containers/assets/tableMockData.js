3f199a2ae536cfb26530529c5ef06d52f144ebe8
mac <mac@macs-iMac.local>
2019-12-10 15:30:50 +0530

    Assets Screen Added

---
+++ src/views/containers/assets/tableMockData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    assetName: "FTM",
+    assetFullName: "Fantom",
+    price: "3,643,255",
+    marketCap: "3,643,255",
+    supply: "$484,201.36",
+    holders: "3,643,255"
+  },
+  {
+    assetName: "XAR",
+    assetFullName: "XAR Network",
+    price: "712,646.55",
+    marketCap: "400,122.65",
+    supply: "$50,999.12",
+    holders: "3,643,255"
+  },
+  {
+    assetName: "USDF",
+    assetFullName: "USDF",
+    price: "23,646.12",
+    marketCap: "400,122.65",
+    supply: "$50,999.12",
+    holders: "3,643,255"
+  }
+];
