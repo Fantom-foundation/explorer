62455eb55f0e090108a99413d8be16b957eb6701
Riya Barua <riya.barua@catalyst.sh>
2019-12-21 14:56:13 +0530

    added cloud function API url

--- src/utils/Utlity.js
+++ src/utils/Utlity.js
@@ -1,3 +1,3 @@
 export const api_get_block = `http://3.136.216.35:3100/api/v1/get-blocks`;
 export const api_get_transactions = `http://3.136.216.35:3100/api/v1/get-transactions`;
-export const api_get_price = `https://api.binance.com/api/v3/ticker/price?symbol=FTMUSDT`;
+export const api_get_price = `https://us-central1-fantom-binance.cloudfunctions.net/getDataFromApi`;
