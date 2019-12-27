7e4a1709deb4d5f8a9a1e0fd4ed30163aadd9a94
Andre Cronje <andre@fantom.foundation>
2019-12-27 15:20:42 +0200

    mainnet details
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- src/utils/Utlity.js
+++ src/utils/Utlity.js
@@ -1,9 +1,9 @@
-export const api_get_block = `http://3.136.216.35:3100/api/v1/get-blocks`;
-export const api_get_transactions = `http://3.136.216.35:3100/api/v1/get-transactions`;
+export const api_get_block = `https://api.fantom.network/api/v1/get-blocks`;
+export const api_get_transactions = `https://api.fantom.network/api/v1/get-transactions`;
 export const api_get_price = `http://ec2-18-216-196-200.us-east-2.compute.amazonaws.com:3000/api/get-price`;
-export const api_get_validators = `http://3.136.216.35:3100/api/v1/staker/?verbosity=2`;
-export const api_get_epoch = `http://3.136.216.35:3100/api/v1/epoch/latest`;
-export const api_get_singleTransaction = `http://3.136.216.35:3100/api/v1/get-transaction?transactionHash=`;
-export const api_get_singleValidators = `http://3.136.216.35:3100/api/v1/staker/id/`;
-export const api_get_singleValidatorsDelegator = `http://3.136.216.35:3100/api/v1/delegator/staker/`;
-export const api_get_address = `http://3.136.216.35:3100/api/v1/get-account?address=`;
+export const api_get_validators = `https://api.fantom.network/api/v1/staker/?verbosity=2`;
+export const api_get_epoch = `https://api.fantom.network/api/v1/epoch/latest`;
+export const api_get_singleTransaction = `https://api.fantom.network/api/v1/get-transaction?transactionHash=`;
+export const api_get_singleValidators = `https://api.fantom.network/api/v1/staker/id/`;
+export const api_get_singleValidatorsDelegator = `https://api.fantom.network/api/v1/delegator/staker/`;
+export const api_get_address = `https://api.fantom.network/api/v1/get-account?address=`;
