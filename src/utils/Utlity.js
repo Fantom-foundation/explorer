86cc8b39f3062060ae01a3f7b90f9ee26eb19d48
Riya Barua <riya.barua@catalyst.sh>
2019-12-27 19:11:07 +0530

    added https to binance link

--- src/utils/Utlity.js
+++ src/utils/Utlity.js
@@ -1,6 +1,6 @@
 export const api_get_block = `https://api.fantom.network/api/v1/get-blocks`;
 export const api_get_transactions = `https://api.fantom.network/api/v1/get-transactions`;
-export const api_get_price = `http://ec2-18-216-196-200.us-east-2.compute.amazonaws.com:3000/api/get-price`;
+export const api_get_price = `https://ec2-18-216-196-200.us-east-2.compute.amazonaws.com:3000/api/get-price`;
 export const api_get_validators = `https://api.fantom.network/api/v1/staker/?verbosity=2`;
 export const api_get_epoch = `https://api.fantom.network/api/v1/epoch/latest`;
 export const api_get_singleTransaction = `https://api.fantom.network/api/v1/get-transaction?transactionHash=`;
